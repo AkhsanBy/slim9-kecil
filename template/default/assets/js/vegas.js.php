801223af61197cf4c7ca271f5479eca957d3ad36
AkhsanBy <akhsanby@gmail.com>
2021-06-06 19:13:02 +0700

    modified slide and add photo

--- template/default/assets/js/vegas.js.php
+++ template/default/assets/js/vegas.js.php
@@ -14,10 +14,10 @@
         transition: '<?= $sysconf['template']['classic_slide_transition']; ?>',
         animation: '<?= $sysconf['template']['classic_slide_animation']; ?>',
         slides: [
-            { src: "<?php echo CURRENT_TEMPLATE_DIR; ?>assets/images/slide1.jpg" },
-            { src: "<?php echo CURRENT_TEMPLATE_DIR; ?>assets/images/slide2.jpg" },
-            { src: "<?php echo CURRENT_TEMPLATE_DIR; ?>assets/images/slide3.jpg" },
-            { src: "<?php echo CURRENT_TEMPLATE_DIR; ?>assets/images/slide4.jpg" }
+            { src: "<?php echo CURRENT_TEMPLATE_DIR; ?>assets/images/slide5.jpg" },
+            { src: "<?php echo CURRENT_TEMPLATE_DIR; ?>assets/images/slide6.jpg" },
+            { src: "<?php echo CURRENT_TEMPLATE_DIR; ?>assets/images/slide7.jpg" },
+            { src: "<?php echo CURRENT_TEMPLATE_DIR; ?>assets/images/slide8.jpg" }
         ]
     });
 </script>
